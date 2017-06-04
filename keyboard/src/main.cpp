6d61640018d1118666c6c02732fb300613c835d6
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2017-06-04 08:04:21 -0500

    Modificados caracteres del keypad

--- keyboard/src/main.cpp
+++ keyboard/src/main.cpp
@@ -2,10 +2,10 @@
 #include <Keypad.h>
 
 char hexaKeys[][4] = {
-  {'0','1','2','3'},
-  {'4','5','6','7'},
-  {'8','9','0','B'},
-  {'C','D','E','F'}
+  {'0','1','2','B'},
+  {'3','4','5','6'},
+  {'E','Z','0','Z'},
+  {'9','8','7','R'}
 };
 
 byte rowPins[] = {11, 10, 9, 8}; //connect to the row pinouts of the keypad
