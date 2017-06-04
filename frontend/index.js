43944f825393e1628cbdc035fed56e8dbaf13eb5
Pedro Palacios Avila <pedro_gpa@hotmail.com>
2017-06-04 12:52:21 -0500

    Ocultado devtools de electron

--- frontend/index.js
+++ frontend/index.js
@@ -18,7 +18,7 @@ function createWindow () {
   }));
 
   // Open the Dev Tools.
-  win.webContents.openDevTools();
+  //win.webContents.openDevTools();
 
   // Emitted when the window is closed.
   win.on('closed', () => {
